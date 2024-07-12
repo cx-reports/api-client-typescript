f5066f159a0314ae3e39b831157623fbf987ffcf
Marko Stijak <marko.stijak@codaxy.com>
2024-07-12 14:59:55 +0200

    Add missing fields

--- src/v1/models/Report.ts
+++ src/v1/models/Report.ts
@@ -1,3 +1,10 @@
 export interface Report {
-    
+  id: number;
+  name: string;
+  reportTypeId: number;
+  reportTypeName: string;
+  reportTemplateName: string;
+  previewImage?: string;
+  themeName?: string;
+  isDefault: boolean;
 }
